2a01d71af1d03faa76d45061440342187e1b1571
dipanshupoonia95211 <dipanshupoonia701@gmail.com>
2026-04-14 23:05:02 +0530

    UC18 : Display Consist Summary

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,30 @@
-import java.util.Arrays;
-
 class TrainConsistManagementApp {
 
     public static void main(String[] args) {
 
         System.out.println("=== Train Consist Management App ===");
 
-        // Array of bogie names
-        String[] bogies = {"Sleeper", "AC Chair", "First Class", "General", "Luxury"};
+        // Array of bogie IDs
+        String[] bogieIds = {"BG101", "BG205", "BG309", "BG412", "BG550"};
 
-        // Sort using built-in method
-        Arrays.sort(bogies);
+        // Search key
+        String target = "BG309";
 
-        // Display sorted array
-        System.out.println("Sorted Bogie Names:");
-        System.out.println(Arrays.toString(bogies));
+        // Linear Search
+        boolean found = false;
+
+        for (String id : bogieIds) {
+            if (id.equals(target)) {
+                found = true;
+                break; // early termination
+            }
+        }
+
+        // Result
+        if (found) {
+            System.out.println("Bogie ID " + target + " found.");
+        } else {
+            System.out.println("Bogie ID " + target + " not found.");
+        }
     }
 }
